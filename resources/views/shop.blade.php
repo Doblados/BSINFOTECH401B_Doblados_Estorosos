1276dfdf1563470d05819f353ace3ed486ed057f
Doblados <147778303+Doblados@users.noreply.github.com>
2024-11-22 21:56:08 +0800

    amen po

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -3,12 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shop</title>
+    <link rel="icon" type="image/png" href="{{ asset('storage/products/irasshai.jpg') }}">
+    <title>Irasshaimase</title>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css" integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <style>
         #productImage {
             max-width: 80%;
             max-height: 300px;
+        }
+
+        .modal-body {
+            text-align: center;
         }
 
         .card-img-top {
@@ -34,14 +43,15 @@
             margin-left: auto;
             margin-right: auto;
         }
+
+        .modal-header {
+            text-align: center;
+        }
     </style>
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css" integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body style="background-color: #E5C9D7 !important;">
     <div class="container my-5">
-        <h1 class="text-center mb-4">Shop</h1>
+        <h1 class="text-center mb-4">Irasshaimase</h1>
 
         @if(session('success'))
             <div class="alert alert-success">
@@ -62,7 +72,7 @@
                     <div class="card h-100" style="border-radius: 10px;" data-bs-toggle="modal" data-bs-target="#productModal"
                          onclick="showProductDetails('{{ $product->id }}', '{{ $product->name }}', '{{ $product->price }}', '{{ asset('storage/' . $product->image) }}', '{{ $product->description }}')">
                         @if($product->image)
-                            <img src="{{ asset('storage/' . $product->image) }}" class="card-img-top" alt="{{ $product->name }}">
+                            <img src="{{ asset('storage/' . $product->image) }}" style="width: 100%;" class="card-img-top" alt="{{ $product->name }}">
                         @else
                             <img src="{{ asset('storage/products/noImg.png') }}" class="card-img-top" alt="No Image">
                         @endif
@@ -76,7 +86,7 @@
         </div>
     </div>
 
-    <!-- Product Details Modal -->
+    <!-- Product Details -->
     <div class="modal fade" id="productModal" tabindex="-1" aria-labelledby="productModalLabel" aria-hidden="true">
         <div class="modal-dialog custom-modal-width modal-lg modal-dialog-centered">
             <div class="modal-content">
@@ -93,12 +103,16 @@
                     <p id="productDescription" class="mt-3"></p>
                 </div>
                 <div class="modal-footer">
-                    <a href="{{ route('update-product', $product->id) }}" class="btn btn-primary" id="updateProductButton">Update</a>
-                    <form action="{{ route('product.destroy', $product->id) }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <input type="hidden" name="_method" value="DELETE">
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                    </form>
+                    @if(isset($product))
+                        <a href="{{ route('update-product', $product->id) }}" class="btn btn-primary" id="updateProductButton">Update</a>
+                        <form action="{{ route('product.destroy', $product->id) }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    @else
+                        <p class="text-danger">Product details are not available.</p>
+                    @endif
                 </div>
             </div>
         </div>
